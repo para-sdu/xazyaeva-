d3bba5370ae12c231c18e724654dc9fcf76ddad4
para-sdu <zhaniyakraman@icloud.com>
2026-05-03 03:53:08 +0500

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,8 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api/v1',
+  baseURL: 'https://azhar-fund.onrender.com/api/v1',
   headers: {
-    // 'Content-Type': 'application/json',
     Accept: 'application/json',
   },
 })
